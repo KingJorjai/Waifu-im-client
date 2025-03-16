6dc93bd4861c4cd9e5459a0eccd5321be61524b2
Jorjai <jorjai@jorjai.net>
2025-03-16 19:58:41 +0100

    Update UI dimensions for waifu.fxml and WaifuApplication; increase StackPane size and adjust scene dimensions for better display, ensuring minimum stage dimensions match the scene size

--- src/main/java/net/jorjai/bummer4/presentation/WaifuApplication.java
+++ src/main/java/net/jorjai/bummer4/presentation/WaifuApplication.java
@@ -12,15 +12,15 @@ public class WaifuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WaifuApplication.class.getResource("waifu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1028, 576);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         WaifuController controller = fxmlLoader.getController();
         stage.setTitle("Waifu.im - Client");
         stage.setScene(scene);
-        stage.setMinHeight(scene.getHeight()/2);
-        stage.setMinWidth(scene.getWidth()/2);
+        stage.setMinHeight(scene.getHeight());
+        stage.setMinWidth(scene.getWidth());
 
+        // Make the image fit the window
         ImageView imageView = controller.getImageView();
-
         stage.heightProperty().addListener((observable, oldValue, newValue) -> {
             imageView.setFitHeight(stage.getHeight());
             imageView.setFitWidth(stage.getWidth());
