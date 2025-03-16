701d716a12a6ee01e8dfa9200098a2ae0a9bbd5c
Jorjai <jorjai@jorjai.net>
2025-03-16 16:21:54 +0100

    Update WaifuController to include UI components: add ComboBox, CheckBox, and Button

--- src/main/java/net/jorjai/bummer4/presentation/WaifuController.java
+++ src/main/java/net/jorjai/bummer4/presentation/WaifuController.java
@@ -1,14 +1,23 @@
 package net.jorjai.bummer4.presentation;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
 
 public class WaifuController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ComboBox<?> categoryComboBox;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private CheckBox nsfwCheckBox;
+
+    @FXML
+    private Button searchButton;
+
 }
